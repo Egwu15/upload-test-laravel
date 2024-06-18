bb07167f8d8e80ce3cdfbe99c30d18ae9edd2c58
Egwu15 <egwutedd@gmail.com>
2024-06-18 22:31:35 +0100

    fixed width issue

--- resources/views/file/uploadImage.blade.php
+++ resources/views/file/uploadImage.blade.php
@@ -31,8 +31,8 @@
             </div>
         @endif
                  
-        <div class="flex justify-center w-screen">
-            <div class=" text-center mx-10">
+        <div class=" md:mx-32">
+            <div class=" text-center">
                 <h2 class="mt-7 text-pink-700 text-2xl font-extrabold">Upload Images</h2>
                 <form class="my-10" action="{{ route('uploadImage') }}" method="post" enctype="multipart/form-data">
                     @csrf
@@ -45,9 +45,9 @@
                     </button>
                 </form>
                 
-                <div class="md:grid md:grid-cols-3 gap-3 flex flex-col items-center justify-center">
+                <div class="md:grid md:grid-cols-3 gap-3 ">
                 @foreach ($files as $file)
-                    <div class=" my-5  border-gray-600 shadow-md w-[80%] md:w-[90%]">
+                    <div class=" py-5  border-gray-600 shadow-md w-[80%] md:w-[90%] mx-auto">
                        
                         <img class="h-[200px] w-full" src="{{ $file->image  }}" alt="sample">
                         <div>
